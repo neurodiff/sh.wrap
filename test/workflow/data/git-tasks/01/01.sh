b89d9e5b5ac3f352c2dd6256fa439d9921c6c4d6
Robin Ragged <78016028+ekotik@users.noreply.github.com>
2022-11-05 20:57:24 +0300

    actions: add git-tasks tests to test workflows run

--- test/workflow/data/git-tasks/01/01.sh
+++ test/workflow/data/git-tasks/01/01.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # shellcheck disable=SC2034
 
-export WORKFLOW_ID="38942442"
+export WORKFLOW_ID="39712949"
 export REF="actions"
 export RUN_ID="git-tasks/01/01"
 export DOCKERFILE_TEMPLATE="./_actions/docker/git-tasks.Dockerfile"
@@ -16,5 +16,4 @@ export GH_HASH="7d71f807c48600d0d8d9f393ef13387504987f1d"
 export GH_BUILD_ARGS=""
 export GIT_REPO="https://github.com/ekotik/sh.wrap"
 export GIT_BRANCH="gh-pages/test"
-export GIT_COMMANDS="git status
-git log"
+export GIT_COMMANDS="git status\\ngit log"
