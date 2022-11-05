6c0435ac783650de46df1eb0b85a3bec7a5f31c8
Robin Ragged <78016028+ekotik@users.noreply.github.com>
2022-11-05 12:40:10 +0300

    fix: actions: test: fix workflow id for pandoc-convert test

--- test/workflow/data/pandoc-convert/01/01.sh
+++ test/workflow/data/pandoc-convert/01/01.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # shellcheck disable=SC2034
 
-export WORKFLOW_ID="38942441"
+export WORKFLOW_ID="39688674"
 export REF="actions"
 export RUN_ID="pandoc-convert/01/01"
 export DOCKERFILE_TEMPLATE="./_actions/docker/pandoc-convert.Dockerfile"
 export DOCKERFILE="pandoc-convert.Dockerfile"
 export WORK_DIR="/github/workspace"
-export SCRIPT="./_actions/src/pandoc-convert.sh"
+export SCRIPT="./_actions/src/org-to-md.sh"
 export IN_DIR="./doc"
 export OUT_DIR="./.doc-out"
 export PANDOC_CLEAN="1"
